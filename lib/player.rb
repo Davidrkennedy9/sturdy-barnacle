b4f87525ba0e73b3ffd84f96eb3e26b1c7a9c095
David Kennedy <davidkennedy@Davids-MacBook-Air.local>
2025-02-12 09:31:30 -0700

    Complete player class

---
+++ lib/player.rb
@@ -0,0 +1,12 @@
+class Player
+    attr_reader :first_name, :last_name, :monthly_cost, :contract_length
+    def initialize(name, cost, length)
+        @first_name = name.split(" ").first
+        @last_name = name.split(" ").last
+        @monthly_cost = cost
+        @contract_length = length
+    end
+    def total_cost
+        monthly_cost * contract_length
+    end
+end
